7e79f1ae5e03f2622daa85962a2d3fa096df1c4c
Artyom <itcoding76@gmail.com>
2023-09-08 18:24:44 +0200

    Block D6 added email verification by registering

--- newsPaperProject/newsPaperProject/settings.py
+++ newsPaperProject/newsPaperProject/settings.py
@@ -173,10 +173,33 @@ AUTHENTICATION_BACKENDS = [
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
-ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_USERNAME_REQUIRED = True
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
-ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
 
 # ============= allauth =====================================
 ACCOUNT_FORMS = {'signup': 'sign.forms.BasicSignupForm'}
+
+
+# ============= send_email ==================================
+
+EMAIL_HOST = 'smtp.yandex.ru' # адрес сервера Яндекс-почты для всех один и тот же
+EMAIL_PORT = 465 # порт smtp сервера тоже одинаковый
+EMAIL_HOST_USER = 'artyom.pv' # ваше имя пользователя, например если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
+EMAIL_HOST_PASSWORD = 'gvyakgemctxwnofg' # пароль от почты
+EMAIL_USE_SSL = True # Яндекс использует ssl, подробнее о том, что это, почитайте на Википедии, но включать его здесь обязательно
+ADMINS = [
+    ('Arty', 'artpv@mail.ru'),
+    # список всех админов в формате ('имя', 'их почта')
+]
+MANAGERS = [
+    ('Arty', 'artpv@mail.ru'),
+    # список всех админов в формате ('имя', 'их почта')
+]
+SERVER_EMAIL = 'artyom.pv@yandex.ru' # это будет у нас вместо аргумента FROM в массовой рассылке
+
+# --------------- setting to email -------------
+
+DEFAULT_FROM_EMAIL = 'artyom.pv@yandex.ru'
+# ============= send_email ==================================
