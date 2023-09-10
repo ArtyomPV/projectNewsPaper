5bc938ed6d099fd50f0145113ef2863d9082e3aa
Artyom <itcoding76@gmail.com>
2023-09-10 09:58:05 +0200

    Block D7.3 added cache for header of base.html, added cache for pages posts and postdetail

--- newsPaperProject/newspaper/urls.py
+++ newsPaperProject/newspaper/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from .views import PostList, PostDetailView, PostCreateView, PostDeleteView, PostUpdateView, CategoryListView, subscribe_to_category, unsubscribe_from_category # импортируем представление
 
+from django.views.decorators.cache import cache_page
+
 app_name = 'newspaper'
 
 urlpatterns = [
-    path('', PostList.as_view(), name='posts'),
+    # path('', PostList.as_view(), name='posts'),
+    path('', cache_page(60)(PostList.as_view()), name='posts'),
     # т.к. сам по себе это класс, то нам надо представить этот класс в виде view. Для этого вызываем метод as_view
-    path('<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    # path('<int:pk>', PostDetailView.as_view(), name='post_detail'),
+    path('<int:pk>', cache_page(300)(PostDetailView.as_view()), name='post_detail'),
     path('create/', PostCreateView.as_view(), name='post_create'),
     path('update/<int:pk>/', PostUpdateView.as_view(), name='post_update'),
     path('delete/<int:pk>/', PostDeleteView.as_view(), name='post_delete'),
